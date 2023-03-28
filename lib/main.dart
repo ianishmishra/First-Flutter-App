06db2cf76b93ab77959d7beff5f3d8402a144573
Anish Mishra <74195045+ianishmishra@users.noreply.github.com>
2023-03-28 15:18:12 +0530

    Day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firstapp8hours/pages/homepage.dart';
 import 'package:firstapp8hours/pages/login_page.dart';
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main(){
   runApp(MyApp());
@@ -17,7 +18,9 @@ class MyApp  extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
