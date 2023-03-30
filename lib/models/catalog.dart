f3422ed7433070e9dfd9f4f2a9f8551b977c40f0
Anish Mishra <74195045+ianishmishra@users.noreply.github.com>
2023-03-30 12:12:15 +0530

    Day 11

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,5 @@
 class CatalogModel {
-  static final items = [
+  static List<Item> items = [
     Item(
       id: 1,
       name: "iphone 14 pro",
@@ -20,5 +20,25 @@ class Item {
   final String image;
 
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+  factory Item.fromMap(Map<String,dynamic> map){
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+
+  toMap() => {
+    "id" : id,
+    "name" : name,
+    "desc" : desc,
+    "price" : price,
+    "color" : color,
+    "image" : image,
+  };
 
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -26,13 +26,17 @@ class _HomePageState extends State<HomePage> {
     final catalogjson = await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogjson);
     final productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
     // print(productsData);
 
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
+    // final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App",
@@ -43,11 +47,11 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
           // itemCount: CatalogModel.items.length,
-          itemCount: dummylist.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context , index) {
             return ItemWidget(
               // item: CatalogModel.items[index],
-              item: dummylist[index],
+              item: CatalogModel.items[index],
             );
           }
         ),
